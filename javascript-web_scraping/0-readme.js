005c554058318587997d18557b086eff8a9d69d3
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2024-01-23 22:55:25 -0400

    fixed bug that may result in code not runninfg correctly

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,9 +2,18 @@
 const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+<<<<<<< HEAD
  if (err) {
   console.error(err);
   return;
  } 
  console.log(data);
 });
+=======
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
+>>>>>>> b8070a5 (fixed bug that may result in code not runninfg correctly)
