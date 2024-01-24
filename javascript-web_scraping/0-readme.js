fe28bc18c19cf7bcea97a540538c701a01dae356
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2024-01-23 22:55:25 -0400

    0 fixed

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf-8', (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log(data);
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+ if (err) {
+  console.error(err);
+  return;
+ } 
+ console.log(data);
 });
