4182d8cbfb3465376f776e7a22ce0370e524a98c
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2024-01-24 15:02:46 -0400

    fixed error preventign code to do function

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -5,7 +5,7 @@ const process = require('process');
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-fs.writeFile(filePaqPath, content, 'utf-8', (err) => {
+fs.writeFile(filePath, content, 'utf-8', (err) => {
   if (err) {
     console.log(err);
   }
